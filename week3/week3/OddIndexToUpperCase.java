b8d52e8b317d8d69fd3a815f86f6b40b87c60bb7
jjabeth <jjabeth@gmail.com>
2024-10-03 17:29:55 +0530

    Add files via upload

---
+++ week3/week3/OddIndexToUpperCase.java
@@ -0,0 +1,25 @@
+package week3.day2_HomeAssignment.String_Assignments;
+/*
+Hints to solve:
+- Use appropriate method to convert the String into a character array.
+- Use the appropriate operator to get the odd index
+- Use toUpperCase() method from the Character class to convert the odd index values to uppercase
+*/
+public class OddIndexToUpperCase {
+	public static void main(String[] args) {
+		 String test = "changeme";
+		 //Convert string to charArray
+		 char[] cs = test.toCharArray();
+		 //loop through each character
+		 for(int i=0;i<=cs.length-1;i++) {
+			 //Find the odd index within the loop
+			 if(i %2 ==1) {
+				 //change the character to uppercase only if the index is odd
+				 System.out.print(Character.toUpperCase(cs[i]));
+			 }else {
+				 System.out.print(cs[i]);
+			 }
+		 }
+	}
+
+}
